3f11a86938ded217ee17ddff26156155b2a96b02
aengun <aengun@icloud.com>
2020-09-14 18:05:04 +0900

    test

--- JavaPrj/src/test.java
+++ JavaPrj/src/test.java
@@ -4,20 +4,19 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
 		int a = 36;
 		int b = 92;
-		
+
 		if (b > a) {
 			int tmp = a;
 			a = b;
 			b = tmp;
 		}
-		
+
 		while (b > 0) {
 			int tmp = b;
-			b = a%b;
-			a = tmp;			
+			b = a % b;
+			a = tmp;
 		}
 		System.out.println(a);
 
